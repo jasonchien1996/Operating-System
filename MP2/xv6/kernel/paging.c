67aa3883099e6a2bb7ab9665ca3983afe37781ad
Jason <43934912+jasonchien1996@users.noreply.github.com>
2022-10-28 19:08:05 +0800

    Add files via upload

---
+++ MP2/xv6/kernel/paging.c
@@ -0,0 +1,42 @@
+#include "param.h"
+#include "types.h"
+#include "memlayout.h"
+#include "riscv.h"
+#include "spinlock.h"
+#include "defs.h"
+#include "proc.h"
+
+/* NTU OS 2022 */
+/* Page fault handler */
+int handle_pgfault() {
+  /* Find the address that caused the fault */
+  uint64 va = r_stval();
+  va = PGROUNDDOWN(va);
+  
+  void *pa = kalloc();
+  memset(pa, 0, PGSIZE);
+    
+  struct proc *p = myproc();
+  pte_t *pte = walk(p->pagetable, va,1);
+  
+  if(*pte&PTE_S){ 
+    uint blockno = (uint)PTE2BLOCKNO(*pte);
+        
+    //swap the page to RAM
+    begin_op();
+    read_page_from_disk(ROOTDEV, pa, blockno);
+    bfree_page(ROOTDEV, blockno);
+    end_op();
+
+    uint64 mask = 0b1111111111000000000000000000000000000000000000000000001111111111;
+    *pte = (*pte&mask)|PA2PTE((uint64)pa);
+    *pte = (*pte ^ PTE_S) | PTE_V;        
+  }
+  else{  
+    if(mappages(p->pagetable, va, PGSIZE, (uint64)pa, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+      kfree(pa);
+      return 0;
+    }
+  }
+  return 1;
+}
